7a9e422098249e98ced53ae368dc70d267fe781e
Prashant Sah <prashantsah1019@gmail.com>
2026-04-18 18:01:17 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smoothWheel: true,
+    smoothTouch: true,
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+window.addEventListener('load', () => {
+    document.documentElement.style.scrollBehavior = 'auto';
+    requestAnimationFrame(raf);
+});
